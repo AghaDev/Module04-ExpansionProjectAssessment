578975e0d373ff75dc6fbdba1f75da1d745f939a
AghaDev <mohamadagha2@gmail.com>
2024-01-08 12:50:35 +0200

    finished the bubble sort and the binary search

---
+++ Algo&DS/bubble-sort.js
@@ -0,0 +1,35 @@
+//Exercise bubble sort
+// const seekAndSwap = (a)=>{
+//     let temp;
+//     for (let i = 0; i < a.length; i++){
+//        for ( j = 0; j < a.length; j++) {
+//         if (a[j] > a[j+1]) {
+//             temp = a[j];
+//             a[j] = a[j+1];
+//             a[j+1] = temp;
+//         }
+//        }
+        
+//     }
+//     return a;
+// }
+
+// console.log(seekAndSwap([5,9,10,12,3,6]));
+
+
+// const N = 0;
+// const arr = [1,2,3,4,5];
+// const K = -1;
+// const binarySearch = (arr ,N ,K) => {
+//     for (let i = 0;i<arr.length;i++){
+//         if(N == arr[i]){
+//                 K = i;
+//                 console.log("Its position is at: "+K);
+//                 break;
+//             }
+//         }
+//        if(K == -1){
+//         console.log("not found");
+//        } 
+//     }
+// binarySearch([1,2,3,4,5,6,10],7,K)
